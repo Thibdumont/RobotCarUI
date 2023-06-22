e8e67cc20860a39803fd57156fff10569a830278
Thibdumont <thibdumont@gmail.com>
2023-06-22 23:23:52 +0200

    compute wifi level from RSSI

--- src/app/components/wifi-signal/wifi-signal.component.ts
+++ src/app/components/wifi-signal/wifi-signal.component.ts
@@ -8,17 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./wifi-signal.component.scss']
 })
 export class WifiSignalComponent {
-  wifiStrength: number = -1;
+  wifiLevel: number = -1;
 
   constructor(
     private robotCommunicationService: RobotCommunicationService
   ) {
     this.robotCommunicationService.robotStateChange.subscribe(robotState => {
-      this.wifiStrength = robotState.wifiStrength;
+      this.wifiLevel = this.getWifiLevel(robotState.wifiStrength);
+    });
+
+    this.robotCommunicationService.connectionStatusChange.subscribe(connectionStatus => {
+      this.wifiLevel = connectionStatus ? 1 : -1;
     });
   }
 
   isBarActive(barNum: number) {
-    return this.wifiStrength >= barNum;
+    return this.wifiLevel >= barNum;
+  }
+
+  getWifiLevel(wifiStrength: number): number {
+    if (wifiStrength > -50) {
+      return 3;
+    }
+    if (wifiStrength > -60) {
+      return 2;
+    }
+    if (wifiStrength > -70) {
+      return 1;
+    }
+    return 0;
   }
 }
